59f027870d6ce0eb064e210ca47969727745d389
Sarah Cheng <saiyuki8820@yahoo.com.tw>
2017-08-07 21:06:58 +0000

    Going to the resturant

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-$(document).ready(function () {
-    var ctx = document.getElementById('canvas').getContext('2d');
+function drawTitle(ctx){
     ctx.font = 'bold 12px sans-serif';
     ctx.fillStyle = '#401e62';
     ctx.fillText('MAKE IT', 7, 30);
     ctx.fillText('FAST!', 15, 45);
+}
+
+$(document).ready(function () {
+    var canvas = $('#canvas');
+    var ctx= canvas.get(0).getContext('2d');
+
+    drawTitle(ctx);
+    canvas.click(function () {
+        canvas.addClass('show-restaurant');
+        ctx.clearRect(0,0,64,64);
+    });
 });
