1281d2e3e83b070fdc719ccfff28bba7e177064c
Sarah Cheng <saiyuki8820@yahoo.com.tw>
2017-08-07 01:53:34 +0000

    Title screen

---
+++ README.md
@@ -0,0 +1,3 @@
+This game is made for #LOWREZJAM 2017.
+
+I hope you'll like it.
---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.font = 'bold 12px sans-serif';
+    ctx.fillStyle = '#401e62';
+    ctx.fillText('MAKE IT', 7, 30);
+    ctx.fillText('FAST!', 15, 45);
+});
